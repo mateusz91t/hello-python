2e1a9885b4dd2b2a64d161503b8c03926d145f54
Mateusz Trzuskowski <mateusz.trzuskowski.91@gmail.com>
2021-07-06 22:48:59 +0200

    Remind #2 to plots

--- ksiazka_py/remind.py
+++ ksiazka_py/remind.py
@@ -1,4 +1,5 @@
 # get ASCII chars
+
 l1 = list()
 for i in range(97, 108):
     l1.append(chr(i))
@@ -29,10 +30,6 @@ s2
 s2.update(['a', 'c'])
 s2
 s2.intersection(s1)
-s2.discard(s1)
-s2
-s1.discard(s2)
-s1
 s2.issubset(s1)
 s2.union(s1)
 s2.difference(s1)
@@ -46,10 +43,11 @@ s2.isdisjoint(s1)
 s2.isdisjoint({'y', 'z'})
 s2.issuperset({'a', 'b', 'c'})
 
+import functools
+import time
 # LRU
 from datetime import datetime
-import time
-import functools
+
 
 @functools.lru_cache(maxsize=None)
 def czek_lru():
@@ -63,25 +61,32 @@ def czek():
 
 
 start = datetime.now()
-for _ in range(5):
+for _ in range(2):
     czek_lru()
 elapsed = datetime.now() - start
 print('czek_lru():\t' + str(elapsed))
 
 start = datetime.now()
-for _ in range(5):
+for _ in range(2):
     czek()
 elapsed = datetime.now() - start
 print('czek():\t' + str(elapsed))
 
-
 #
 import os
-os.getcwd()
+
+cur_dir = os.getcwd()
+cur_dir
+airtravel = 'files_sources/airtravel.csv'
+if cur_dir != 'D:\\Mateusz\\nauka\\python\\hello':
+    airtravel = '../' + airtravel
+airtravel
 
 import pandas as pd
-df = pd.read_csv('files_sources/airtravel.csv')
+
+df = pd.read_csv(airtravel)
 df.head(1)
+df.columns = ['Month', 1958, 1959, 1960]
 df.tail(1)
 df.iloc[0]
 df.iloc[-1]
@@ -90,9 +95,10 @@ df.size
 df.shape[0] * df.shape[1]
 
 
-def get_row_by_row(df:pd.DataFrame):
+def get_row_by_row(df: pd.DataFrame):
     for i in range(len(df)):
         yield df.iloc[i]
+
 
 at2 = pd.DataFrame()
 for row in get_row_by_row(df):
@@ -101,3 +107,108 @@ at2
 at2 = pd.DataFrame()
 at_gen = get_row_by_row(df)
 next(at_gen)
+
+
+# decorators
+def decor(func):
+    print('before')
+
+    def internal_f(x):
+        x = x.lower()
+        print('internal before')
+        func(x)
+        print('internal after')
+
+    print('after')
+    return internal_f
+
+
+def print_name(name: str):
+    print(name.swapcase())
+
+
+@decor
+def print_name_decor(name: str):
+    print(name.swapcase())
+
+
+print_name('Jarek')
+
+print_name_decor('Jarek')
+
+# os
+import os
+
+os.getcwd()
+dirs = os.walk(os.getcwd())
+next(dirs)
+os.listdir()
+os.path.exists('../files_sources/')
+os.path.exists('files_sources/')
+os.path.isdir('../files_sources/')
+os.path.isdir('files_sources/')
+os.path.isfile('files_sources/')
+os.path.getsize(airtravel)
+# mkdir, rmdir, remove...
+
+
+# subprocess
+import subprocess
+
+result = subprocess.call(['ping', '-n', '2', 'onet.pl'])
+result  # if ok returns 0, if an error returns 1
+result = subprocess.Popen(['ping', '-n', '2', 'onet.pl'])
+result.communicate()
+
+# faker lib
+import faker
+
+f = faker.Faker()
+f.name()
+f.sentence()
+f.date()
+f.time()
+f.future_datetime()
+f.word()
+f.email()
+f.free_email()
+f.free_email_domain()
+f.name_male()
+f.name_female()
+f.first_name_female()
+f.firefox()
+f.first_name_nonbinary()
+f.fixed_width()
+
+f.__dict__
+
+f.locales
+f.seed_locale('en_US')
+f.factories.insert(2,2)
+f.factories.pop()
+f.random.randrange(20)
+
+
+# plots
+# common linear plots
+from matplotlib import pyplot as plt
+
+plt.style.use('dark_background')
+df.plot(grid=True)
+df
+
+df.iloc[:, 1]
+
+plt.plot(df.iloc[:, 1], '-', label='1958')
+plt.plot(df.iloc[:, 2], '--')
+plt.plot(df.iloc[:, 3], ':')
+
+plt.xlabel('Month')
+plt.ylabel('count')
+plt.grid()
+df.columns[1:]
+df.columns[:0:-1]
+plt.legend(df.columns[1:])
+
+
+# todo bar/hist plots
